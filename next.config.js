b5c6edc7c79c2bc4888fe6eb7bf5dbbc6ecff5b0
Zoran Luledzija <zoran.luledzija@outlook.com>
2022-03-12 16:42:37 +0100

    Add config for internationalized routing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["en", "fr", "nl-NL"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "en",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
