edad4091a71756ec7c90796573268e3ff6956d9f
Zoran Luledzija <zoran.luledzija@outlook.com>
2022-03-14 17:10:53 +0100

    Add localization files and set up IntlProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
+
+import en from "../lang/en.json";
+import fr from "../lang/fr.json";
+import nl_NL from "../lang/nl-NL.json";
+
+import "../styles/globals.css";
+
+const messages = {
+  en,
+  fr,
+  "nl-NL": nl_NL,
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { locale } = useRouter();
+
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <Component {...pageProps} />
+    </IntlProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
